9c5d7c6e274c750f4b74e273fe5873141f9e9bcb
Sara Li <ruimin.li77@gmail.com>
2023-07-21 01:36:32 -0700

    Initial commit for Heroku deployment

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app = require('./index');
 // app.listen(PORT, () => {
 //   console.log(`Listening at http://localhost:${PORT}`);
 // });
-app.set('PORT', (process.env.PORT || 5001));
+app.set('port', (process.env.PORT || 5001));
 
 // Start node server
 app.listen(app.get('port'), () => {
