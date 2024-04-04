b5b6d4c3c8cb0d2fa8f0b23fe14a5f829312be0a
Sara Li <ruimin.li77@gmail.com>
2024-04-04 01:24:32 -0700

    Add TRY IT AS A GUEST feature

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('../../database/controllers/userControllers');
 const userDataController = require('../../database/controllers/userDataControllers');
 const problemsController = require('../../database/controllers/problemsControllers');
+
 const router = express.Router();
 
 router.get('/problems/titles', problemsController.getAllProblemsTitles);
@@ -12,6 +13,7 @@ router.post('/register', userController.createNewUser);
 router.post('/login', userController.userLogin);
 
 router.get('/users/:id', userDataController.getUserDataByUserId);
+router.get('/demo', userDataController.getDemoAcc);
 router.post('/users/:id/lists', userDataController.addProblemsToLists);
 router.put('/users/:id/lists/delete', userDataController.deleteProblem);
 router.put('/users/:id/lists/checkmark', userDataController.updateCheckmark);
